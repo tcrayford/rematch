39f12f94690898e5d6fa409ac6d6981fa30b87c3
Tom Crayford <tcrayford@googlemail.com>
2013-01-10 11:48:42 +0000

    rewrite core types for the last time, use hamcrest style output strings

--- Control/Rematch.hs
+++ Control/Rematch.hs
@@ -5,23 +5,40 @@ data Match = MatchSuccess | MatchFailure String deriving (Eq, Show)
 
 data Matcher a = Matcher {
     match :: a -> Bool
-  , describeMatch :: String
+  , description :: String
+  , describeMismatch :: a -> String
   }
 
+-- XXX: rework
 hasItem :: (Eq a, Show a) => a -> Matcher [a]
-hasItem a = Matcher (a `elem`) $ show a ++ " to be in"
+hasItem a = Matcher (a `elem`) "has" (\b  -> "was " ++ show b)
 
 assertThat :: (Show a) => a -> Matcher a -> Assertion
 assertThat a matcher = (runMatch matcher) a @?= MatchSuccess
 
 no :: Matcher a -> Matcher a
-no (Matcher m description) = Matcher go ("Not " ++ description)
-  where go a = not $ m a
+no (Matcher m desc mismatch) = Matcher (not . m) ("not " ++ desc) (\a -> (mismatch a))
 
-runMatch :: (Show a) => Matcher a -> a -> Match
+runMatch :: Matcher a -> a -> Match
 runMatch m a = if (match m $ a)
   then MatchSuccess
-  else MatchFailure ("Expected: " ++ describeMatch m ++ " " ++ show a)
+  else MatchFailure $ "Expected:\n " ++ description m ++ "\n  but:  " ++ (describeMismatch m $ a)
 
 is :: (Show a, Eq a) => a -> Matcher a
-is a = Matcher (a == ) $ show a ++ " to equal"
+is a = Matcher (a == ) ("equalTo " ++ show a) (\b -> "was " ++ show b)
+
+allOf :: (Show a, Eq a) => [Matcher a] -> Matcher a
+allOf [] = Matcher (const False) "allOf" (const "was: no matchers supplied")
+allOf matchers = Matcher {
+    match = (\a -> and $ map (\m -> match m a) matchers)
+  , description = describeList "all" $ map description matchers
+  , describeMismatch = (\a -> describeList "was" (map ((flip describeMismatch) a) (filter (\m -> not $ match m $ a) matchers)))
+  }
+
+describeList :: String -> [String] -> String
+describeList start xs = start ++ "(" ++ join ", " xs ++ ")"
+
+join :: String -> [String] -> String
+join _ [] = ""
+join _ [a] = a
+join sep (x:xs) = x ++ sep ++ join sep xs
--- Control/Rematch/Specs.hs
+++ Control/Rematch/Specs.hs
@@ -7,28 +7,52 @@ import Control.Rematch
 main :: IO ()
 main = hspec $ do
   describe "rematch core" $ do
-    it "passes" $ do
-      runMatch (hasItem 'a') "a" @?= MatchSuccess
-
-    it "describes failure" $
-      runMatch (hasItem 'a') "b" @?= MatchFailure "Expected: 'a' to be in \"b\""
-
     it "can be inverted" $
-      runMatch (no (hasItem 'a')) "b" @?= MatchSuccess
+      checkMatch (no (is 'a')) 'b' @?= Nothing
 
     it "has a failure message when inverted" $
-      runMatch (no (hasItem 'a')) "a" @?= MatchFailure "Expected: Not 'a' to be in \"a\""
+      checkMatch (no (is 'a')) 'a' @?= Just ("not equalTo 'a'", "was 'a'")
 
   describe "assertThat" $ do
     it "is used as an hunit test" $
       assertThat "a" (hasItem 'a')
 
   generalPurposeMatcherSpecs
+  combinedMatcherSpecs
+  joinSpecs
 
 generalPurposeMatcherSpecs :: Spec
 generalPurposeMatcherSpecs = describe "general purpose matchers" $ do
   describe "is" $ do
     it "matches on equality" $
-      runMatch (is 'a') 'a' @?= MatchSuccess
+      checkMatch (is 'a') 'a' @?= Nothing
     it "fails on inequality" $
-      runMatch (is 'b') 'a' @?= MatchFailure "Expected: 'b' to equal 'a'"
+      checkMatch (is 'b') 'a' @?= Just ("equalTo 'b'", "was 'a'")
+
+combinedMatcherSpecs :: Spec
+combinedMatcherSpecs = describe "combined matchers" $ do
+  describe "allOf" $ do
+    it "matches when all of the individual matchers match" $
+      checkMatch (allOf [is 'a', is 'a']) 'a' @?= Nothing
+
+    it "fails when you don't supply a matcher at all" $
+      checkMatch (allOf []) 'a' @?= Just ("allOf", "was: no matchers supplied")
+
+    it "fails if an individual matcher matches" $
+      checkMatch (allOf [is 'a', is 'b']) 'a' @?= Just ("all(equalTo 'a', equalTo 'b')","was(was 'a')")
+
+joinSpecs :: Spec
+joinSpecs = describe "join" $ do
+  it "doesn't do anything for an empty list" $
+    join "a" [] @?= ""
+
+  it "just displays a single elements list" $
+    join ", " ["a"] @?= "a"
+
+  it "joins a list of two items with the separator" $
+    join ", " ["a", "b"] @?= "a, b"
+
+checkMatch :: Matcher a -> a -> Maybe (String, String)
+checkMatch m a = if (match m $ a)
+  then Nothing
+  else Just (description m, describeMismatch m $ a)
