86e765af365537e563951c975123d4d783b183de
Tom Crayford <tcrayford@googlemail.com>
2013-05-19 17:27:34 +0100

    implement ByteString.Char8 matchers

--- bytestring-rematch/Control/Rematch/ByteString/Specs.hs
+++ bytestring-rematch/Control/Rematch/ByteString/Specs.hs
@@ -1,15 +1,48 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Control.Rematch.ByteString.Specs where
 import Test.Hspec
 import Test.HUnit
 import Test.Hspec.HUnit()
 import Control.Rematch
 import qualified Control.Rematch.ByteString.Strict as S
+import Data.ByteString.Char8
 
 main :: IO ()
 main = hspec $ describe "rematch-bytestring" $ do
   describe "Control.Rematch.ByteString.Strict" $ do
-    it "" $
-      pending "todo"
+    describe "startsWith" $ do
+      it "passes when the input begins with the query" $
+        checkMatch "abcd" (S.startsWith "ab") @?= Nothing
+
+      it "fails when the input does not being with the query" $
+        checkMatch "zyx" (S.startsWith "ab") @?= Just ("startsWith \"ab\"", "was \"zyx\"")
+
+    describe "endsWith" $ do
+      it "matches when the input ends with the query" $
+        checkMatch "abcd" (S.endsWith "cd") @?= Nothing
+
+      it "matches when the input does not end with the query" $
+        checkMatch "abc" (S.endsWith "cd") @?= Just ("endsWith \"cd\"", "was \"abc\"")
+
+    describe "containsText" $ do
+      it "matches when the input contains the text" $
+        checkMatch "abcd" (S.containsText "bc") @?= Nothing
+
+      it "fails when the input does not contain the text" $
+        checkMatch "abd" (S.containsText "bc") @?= Just ("containsText \"bc\"", "was \"abd\"")
+
+    describe "isEmptyByteString" $ do
+      it "matches when the input is empty" $
+        checkMatch "" S.isEmptyByteString @?= Nothing
+
+      it "fails when the input has content" $
+        checkMatch "asdf" S.isEmptyByteString @?= Just ("isEmptyByteString", "was \"asdf\"")
+
   describe "Control.Rematch.ByteString.Lazy" $ do
     it "" $
       pending "todo"
+
+checkMatch :: a -> Matcher a -> Maybe (String, String)
+checkMatch a m = if match m a
+  then Nothing
+  else Just (description m, describeMismatch m a)
--- bytestring-rematch/Control/Rematch/ByteString/Strict.hs
+++ bytestring-rematch/Control/Rematch/ByteString/Strict.hs
@@ -1,1 +1,31 @@
 module Control.Rematch.ByteString.Strict where
+import Control.Rematch(Matcher(..), standardMismatch)
+import qualified Data.ByteString.Char8 as B
+
+startsWith :: B.ByteString -> Matcher B.ByteString
+startsWith b = Matcher {
+    match = B.isPrefixOf b
+  , description = "startsWith " ++ show b
+  , describeMismatch = standardMismatch
+  }
+
+endsWith :: B.ByteString -> Matcher B.ByteString
+endsWith b = Matcher {
+    match = B.isSuffixOf b
+  , description = "endsWith " ++ show b
+  , describeMismatch = standardMismatch
+  }
+
+containsText :: B.ByteString -> Matcher B.ByteString
+containsText b = Matcher {
+    match = B.isInfixOf b
+  , description = "containsText " ++ show b
+  , describeMismatch = standardMismatch
+  }
+
+isEmptyByteString :: Matcher B.ByteString
+isEmptyByteString = Matcher {
+    match = B.null
+  , description = "isEmptyByteString"
+  , describeMismatch = standardMismatch
+  }
